f0918fe4ffa14c3d2ba278e6bd34eb0ad6dbf477
Elijah Ducote <ducote.help@gmail.com>
2024-01-17 14:48:37 -0600

    Message

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ vlite = new Vlitejs(feed,{
 tym,
 lazy = document.getElementById("loader"),
 player;
-window.Vlitejs.registerProvider("youtube", window.VlitejsYoutube);
 function throttle (func, delay) {
 var prev = 0;
 return function () {
@@ -109,18 +108,19 @@ for (var name = ["ult","pnult","alph"];nth;--nth) {
 items[nth - 1] = root.getElementsByClassName(name[nth - 1])[0];
 }
 items[0].addEventListener("click", throttle(function (e) {
-active_slot = e.target;
+active_slot = items[0];
+;
 menu.val = 0;
 items[2].style.inset = "0";
 items[1].style.inset = "0";
 items[0].style.inset = "3.625em -.625em";
 },4096));
 items[1].addEventListener("click",throttle(function (e) {
-active_slot = e.target;
+active_slot = items[1]
 press(1);
 },3072));
 items[2].addEventListener("click",throttle(function (e) {
-active_slot = e.target;
+active_slot = items[2];
 press(2);
 },2048));
 }
--- vanjs.js
+++ vanjs.js
@@ -1,7 +1,11 @@
+window.Vlitejs.registerProvider("youtube", window.VlitejsYoutube);
 var van = this.van,
 menu = van.state(0),
 view = document.getElementById("app"),
-loadr = document.getElementById("loader");
+loadr = document.getElementById("loader"),
+playr,
+evnt,
+playback;
 function frag (name) {
 return van.tags[name];
 }
@@ -22,22 +26,19 @@ function ux () {
   br = frag("br"),
   b = frag("b"),
   p = frag("p"),
+  iframe = frag("iframe"),
   h1 = frag("h1"),
   stripe = frag("stripe-buy-button"),
   audio = frag("audio"),
   source = frag("source"),
   img = frag("img"),
   blockquote = frag("blockquote"),
-  player = document.getElementsByClassName("player")[0],
   yt = frag("svg")({class:"brand"}),
   ico = frag("svg")(),
   base = div({class:"wrapper"});
   ico.innerHTML = r;
   base.style.zIndex = "0";
   yt.innerHTML = "<svg id=title xmlns=http://www.w3.org/2000/svg width=256 height=256 preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 256 256\"><defs><clipPath id=a clipPathUnits=userSpaceOnUse><path d=\"M0 192h192V0H0Z\"/></clipPath></defs><g clip-path=\"url(#a)\" transform=\"matrix(1 0 0 -1 0 256)\"><path fill=\"red\" d=\"M180 139c-2 7-8 13-15 15-14 4-69 4-69 4s-55 0-69-4c-7-2-13-8-15-15-4-14-4-43-4-43s0-29 4-43c2-7 8-13 15-15 14-4 69-4 69-4s55 0 69 4c7 2 13 8 15 15 4 14 4 43 4 43s0 29-4 43\"/></g></svg>";
- ico.addEventListener("click",function(){
-   player.play();
- });
   van.add(base,blockquote({class:"blockquote"},sel([span("Few’s ",yt,span({style:"vertical-align:super;font-size:.5em"},"(dot) "),span("Tube"),span("\n\n\"Us Few, our "),span({style:"color:#F00"},"‘Tube.\"")),span("Support Us"),span("Episode Catalog")])));
   setTimeout(function (){
    loadr.style.visibility = "none";
@@ -46,6 +47,10 @@ function ux () {
  },1400);
   if (menu.val === 0) {
   van.add(base,br(),div({class:"card"},div({class:"card-body"},div({class:"card-header"},h1("Welcome!")),div({class:"card-subtitle"},br(),p({style:"color:#e7ee46"},"About Us"),img({class:"card-img",src:"./fewstube.png"}),hr({class:"hr"}),div({class:"card-text"},p(b("The Trifect"),ico,span({style:"color:#1bdda6"},"\n(/ðə/ \n/ˈtɹaɪˌfɛkt/)"),audio({class:"player",preload:"auto"},source({type:"audio/mpeg",src:"./voice.mp3"})),"\n…is an interactive podcast/​audiodrama\n-esque series, informally a \"not-cast\", with added visual aid that discusses topics of all kinds like the elements of animation, personal identity, & exploring the soporose of minds' desires."))))));
+  playback = base.getElementsByClassName("player")[0];
+  evnt = ico.addEventListener("click",function(){
+   playback.play();
+ });
   return base;
   }
   if (menu.val === 1) {
@@ -53,11 +58,11 @@ function ux () {
   return base;
   }
   if (menu.val === 2) {
-    van.add(base,div({class:"text-danger"},"No episodes yet"),div({class:"yt",id:"ytplay"}));
-    var after = base.getElementsByClassName("yt")[0];
-    after.dataset["youtubeId"] = "_KDUICa4UCM";
-    console.log(after)
-    var video = new Vlitejs(after,{options:{
+    van.add(base,div({class:"text-danger"},"No episodes yet"));
+    van.add(base,div({id:"ytb",class:"vid"}));
+    var ytb = base.getElementsByClassName("vid")[0];
+    ytb.dataset["youtubeId"] = "_KDUICa4UCM";
+   var player = new Vlitejs(ytb,{options:{
     controls: true,
     autoplay: true,
     playPause: true,
@@ -65,7 +70,7 @@ function ux () {
     time: true,
     volume: true,
     fullscreen: true,
-    poster:"https://i3.ytimg.com/vi/" + after.dataset["youtubeId"] + "/maxresdefault.jpg",
+    poster:"https://i3.ytimg.com/vi/" + ytb.dataset["youtubeId"] + "/maxresdefault.jpg",
     bigPlay: true,
     playsinline: true,
     loop: true,
@@ -78,17 +83,20 @@ function ux () {
       playsinline:1,
       controls:0,
       iv_load_policy:3,
+      origin:"http://localhost:8158",
       rel:0,
       loop:1
     }},
     onReady:function(p){
+      playr = p.getInstance();
+     // playr.destroy();
+      p.play();
       p.unMute();
     },
-     provider:["youtube"]
-    });
+     provider:"youtube"
+   });
     return base;
-  }
-  
+  };
  //var after = base.getElementsByClassName("card")[0].firstElementChild;
  // if (menu.val === 2) van.add(after,h1("help"));
   return base;
@@ -100,5 +108,7 @@ function Page () {
 van.add(view,Page);
 
 van.derive(function () {
+  console.log(window.VlitejsYoutubeQueue);
+  if (evnt) playback.removeEventListener(evnt);
   van.hydrate(view,Page);
 });
