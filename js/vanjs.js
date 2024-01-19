ab824b325c327f900ed36a6f37a44b1a50fc4674
Alendrone <33103761+Alendrone@users.noreply.github.com>
2024-01-19 12:47:32 -0600

    update js/vanjs.js

--- js/vanjs.js
+++ js/vanjs.js
@@ -90,8 +90,9 @@ function ux() {
       },
       onReady: function(p) {
         p.on("progress", function () {
-        var video = document.getElementsByClassName("v-media")[0];
-        van.hydrate(video,iframe({id:"ytb",class:"vid v-media","data-youtube-id":"_KDUICa4UCM",frameborder:"0",allowfullscreen:"",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",title:"&quot;Structural Integrity&quot; | The Trifect #shorts_video",width:"480",height:"640",src:"https://www.youtube.com/embed/_KDUICa4UCM?autohide=1&amp;controls=0&amp;fs=1&amp;modestbranding=0&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=transparent&amp;autoplay=1&amp;start=0&amp;end=60&amp;iv_load_policy=3&amp;loop=1&amp;enablejsapi=1&amp;widgetid=1" ,tabindex:"-1"}));
+        var video = document.getElementsByClassName("v-media")[0].parentNode,
+        elem = iframe({id:"ytb",class:"vid v-media","data-youtube-id":"_KDUICa4UCM",frameborder:"0",allowfullscreen:"",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",title:"&quot;Structural Integrity&quot; | The Trifect #shorts_video",width:"480",height:"640",src:"https://www.youtube.com/embed/_KDUICa4UCM?autohide=1&amp;controls=0&amp;fs=1&amp;modestbranding=0&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=transparent&amp;autoplay=1&amp;start=0&amp;end=60&amp;iv_load_policy=3&amp;loop=1&amp;enablejsapi=1&amp;widgetid=1" ,tabindex:"-1"}));
+        video.innerHTML = elem,
         });
         p.play();
         p.unMute();
